e060db1176dd5e0be90445825d0cc47a5e51bbc2
SHOBHIT SHRIVASTAVA <111689849+Shobhit51@users.noreply.github.com>
2024-06-03 21:11:21 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import p1 from '../images/p1.png';
 import p2 from '../images/p2.png';
-
+import VerticalTimeline from '../components/VerticalTimeline';
 function About() {
   return (
     <div>
@@ -17,30 +17,7 @@ function About() {
             <p className="leading-relaxed font-serif mb-4 text-lg ">
               Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY.
             </p>
-            <div className="relative pl-8 sm:pl-32 py-6 group text-lg ">
-              <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                <time className=" text-lg  sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold font-serif uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2021</time>
-              </div>
-              <div className="text-slate-500 font-serif text-lg " >
-                Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-              </div>
-            </div>
-            <div className=" relative pl-8 sm:pl-32 py-6 group">
-              <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                <time className=" text-lg  sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase  font-serif w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2022</time>
-              </div>
-              <div className="text-slate-500 font-serif text-lg " >
-                Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-              </div>
-            </div>
-            <div className="relative pl-8 sm:pl-32 py-6 group">
-              <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                <time className=" text-lg  sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20  font-serif h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2023</time>
-              </div>
-              <div className="text-slate-500 font-serif text-lg " >
-                Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-              </div>
-            </div>
+            <VerticalTimeline />
           </div>
         </div>
       </section>
@@ -57,22 +34,12 @@ function About() {
                 <div className="leading-relaxed mb-4 text-lg ">
                   Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
                 </div>
-                   <div className="leading-relaxed mb-4 text-lg ">
+                 <div className="text-slate-500 font-serif text-lg " >
                   Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
                 </div>
-                   <div className="leading-relaxed mb-4 text-lg ">
+                <div className="text-slate-500 font-serif text-lg " >
                   Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
                 </div>
-                   <div className="leading-relaxed mb-5 text-lg ">
-                  Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-                </div>
-                   <div className="leading-relaxed mb-4 text-lg ">
-                  Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-                </div>
-                   <div className="leading-relaxed mb-4 text-lg ">
-                  Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-                </div>
-                
               </div>
             </div>
           </div>
