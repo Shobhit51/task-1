57fd16a6bc470d14232d6a5bbec8f3fdd6f4d4c3
SHOBHIT SHRIVASTAVA <111689849+Shobhit51@users.noreply.github.com>
2024-06-03 14:54:41 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,37 +6,38 @@ function About() {
   return (
     <div>
       <section className="text-gray-600 body-font">
+        
         <div className="container px-5 py-24 mx-auto flex flex-wrap">
           <div className="lg:w-1/2 w-full mb-10 lg:mb-0">
           <img className=" w-full h-full object-cover object-center" src={p1} alt="person" />
 
           </div>
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h1 className="text-black text-3xl title-font font-medium mb-4">History</h1>
-            <p className="leading-relaxed mb-4 ">
+            <h1 className="text-black  text-3xl mb-4   font-medium font-serif"> Our History</h1>
+            <p className="leading-relaxed font-serif mb-4 text-lg ">
               Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY.
             </p>
-            <div className="relative pl-8 sm:pl-32 py-6 group">
+            <div className="relative pl-8 sm:pl-32 py-6 group text-lg ">
               <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2021</time>
+                <time className=" text-lg  sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold font-serif uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2021</time>
               </div>
-              <div className="text-slate-500">
+              <div className="text-slate-500 font-serif text-lg " >
+                Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
+              </div>
+            </div>
+            <div className=" relative pl-8 sm:pl-32 py-6 group">
+              <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                <time className=" text-lg  sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase  font-serif w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2022</time>
+              </div>
+              <div className="text-slate-500 font-serif text-lg " >
                 Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
               </div>
             </div>
             <div className="relative pl-8 sm:pl-32 py-6 group">
               <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2022</time>
+                <time className=" text-lg  sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20  font-serif h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2023</time>
               </div>
-              <div className="text-slate-500">
-                Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-              </div>
-            </div>
-            <div className="relative pl-8 sm:pl-32 py-6 group">
-              <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2023</time>
-              </div>
-              <div className="text-slate-500">
+              <div className="text-slate-500 font-serif text-lg " >
                 Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
               </div>
             </div>
@@ -46,20 +47,20 @@ function About() {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto flex flex-wrap">
           <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mt-6 lg:mt-0">
-            <h1 className="text-black  text-3xl title-font font-medium mb-4">Vision</h1>
-            <p className="leading-relaxed mb-4">
+            <h1 className="text-black  text-3xl mb-4   font-medium font-serif">Vision</h1>
+            <p className="text-slate-500 font-serif text-lg " >
               Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY.
             </p>
             <div className="relative pl-8 sm:pl-20 py-6 group">
               <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5"></div>
-              <div className="text-slate-500">
-                <div className="leading-relaxed mb-4">
+              <div className="text-slate-500 font-serif" >
+                <div className="leading-relaxed mb-4 text-lg ">
                   Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
                 </div>
-                <div className="leading-relaxed mb-4">
+                 <div className="text-slate-500 font-serif text-lg " >
                   Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
                 </div>
-                <div className="leading-relaxed mb-4">
+                <div className="text-slate-500 font-serif text-lg " >
                   Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
                 </div>
               </div>
@@ -71,89 +72,9 @@ function About() {
           </div>
         </div>
       </section>
+
     </div>
   );
 }
 
 export default About;
-// import React from 'react';
-// import p1 from '../images/p1.png';
-// import p2 from '../images/p2.png';
-
-// function About() {
-//   return (
-//     <div>
-//    <section className="text-gray-600 body-font">
-//   <div className="container px-5 py-24 mx-auto flex flex-wrap">
-//     <div className="lg:w-1/2 w-full mb-10 lg:mb-0">
-//     <img className="rounded-full w-32 h-32 object-cover object-center" src= {p1} alt="person"> </img>
-//       {/* <img alt="ecommerce" className="object-cover object-center rounded-lg" src="https://dummyimage.com/800x600"> */}
-//     </div>
-//     <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-//       <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">History</h1>
-//       <p className="leading-relaxed mb-4">
-//         Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY.
-//       </p>
-//       <div className="relative pl-8 sm:pl-32 py-6 group">
-//         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-//           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2021</time>
-//         </div>
-//         <div className="text-slate-500">
-//           Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-//         </div>
-//       </div>
-//       <div className="relative pl-8 sm:pl-32 py-6 group">
-//         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-//           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2022</time>
-//         </div>
-//         <div className="text-slate-500">
-//           Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-//         </div>
-//       </div>
-//       <div className="relative pl-8 sm:pl-32 py-6 group">
-//         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-//           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">2023</time>
-//         </div>
-//         <div className="text-slate-500">
-//           Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </section>
-// <section className="text-gray-600 body-font">
-//   <div className="container px-5 py-24 mx-auto flex flex-wrap">
-//     <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mt-6 lg:mt-0">
-//       <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">Vision</h1>
-//       <p className="leading-relaxed mb-4">
-//         Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY.
-//       </p>
-//       <div className="relative pl-8 sm:pl-20 py-6 group">
-//         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5"></div>
-//         <div className="text-slate-500">
-//           <div className="leading-relaxed mb-4">
-//             Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-//           </div>
-//           <div className="leading-relaxed mb-4">
-//             Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-//           </div>
-//           <div className="leading-relaxed mb-4">
-//             Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//     <div className="lg:w-1/2 w-full mb-10 lg:mb-0">
-//     <img className="rounded-full w-32 h-32 object-cover object-center" src={p2} alt="person"> </img>
-//       {/* <img alt="ecommerce" className="object-cover object-center rounded-lg" src="https://dummyimage.com/800x600"> */}
-//     </div>
-//   </div>
-// </section>
-
-
-
-// </div>
-//   );
-// }
-
-// export default About;
