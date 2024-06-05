e8ae51098e963edf6327f8d3f05ba94b4b8c255b
SHOBHIT SHRIVASTAVA <111689849+Shobhit51@users.noreply.github.com>
2024-06-05 10:09:53 +0530

    Update VerticalTimeline.jsx

--- src/components/VerticalTimeline.jsx
+++ src/components/VerticalTimeline.jsx
@@ -17,6 +17,11 @@ const timelineData = [
     description:' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque accusamus impedit fugiat commodi, culpa dolorum quas eveniet quis in harum, perspiciatis placeat quibusdam. Nisi maiores animi illum dolore minima doloremque',
   
   },
+  {
+    year: '2021',
+    description:' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque accusamus impedit fugiat commodi, culpa dolorum quas eveniet quis in harum, perspiciatis placeat quibusdam. Nisi maiores animi illum dolore minima doloremque',
+  
+  },
 ];
 
 const VerticalTimeline = () => {
@@ -29,7 +34,7 @@ const VerticalTimeline = () => {
             {/* Year Column */}
             <div className="col-start-1 col-end-2 md:mx-auto relative">
               <div className="h-full w-full flex items-center justify-center">
-              <div className="text-xl font-playfair font bold">{item.year}</div>
+              <div className="text-xl font-playfair font bold pb-20">{item.year}</div>
             
               </div>
             </div>
@@ -38,13 +43,13 @@ const VerticalTimeline = () => {
             {/* Marker Column */}
             <div className="col-start-2 col-end-3 md:mx-auto relative">
               <div className="h-full w-6 flex items-center justify-center">
-                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+              {index === timelineData.length - 1 ? null : <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>}
               </div>
-              <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-gray-700 shadow"></div>
+              <div className="w-6 h-6 absolute top-1 -mt-3 pt-5 rounded-full bg-gray-500 shadow"></div>
             </div>
 
             {/* Event Content Column */}
-               <div className="col-start-3 col-end-12 p-4">
+            <div className="col-start-3 col-end-12 ">
              
               <p className="leading-tight text-justify font-sans">{item.description}</p>
             </div>
