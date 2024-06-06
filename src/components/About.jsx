09c40acd1397c9fe4e24c1df062ba94a30594506
SHOBHIT SHRIVASTAVA <111689849+Shobhit51@users.noreply.github.com>
2024-06-06 22:44:37 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ function About() {
 
           </div>
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h1 className="text-black text-3xl mb-4 pb-2 font-medium font-playfair" style={{ fontFamily: 'Playfair Display', fontSize: '30px', fontWeight: 600, lineHeight: '40px', textAlign: 'left', color: '#4D4E54' }}>
+            <h1  className="text-black text-3xl mb-4 pb-2 font-medium font-playfair text-left">
   Our History
 </h1>
 
