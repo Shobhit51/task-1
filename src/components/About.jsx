2660232e4238e579fe3361d4bfbe5a4147c21912
SHOBHIT SHRIVASTAVA <111689849+Shobhit51@users.noreply.github.com>
2024-06-06 22:41:01 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,30 +25,42 @@ function About() {
 
           </div>
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h1 className="text-black  text-3xl mb-4   font-medium  font-playfair "> Our History</h1>
-            <p className="leading-relaxed font-poppins mb-4 text-lg ">
-              Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY.
-            </p>
+            <h1 className="text-black text-3xl mb-4 pb-2 font-medium font-playfair" style={{ fontFamily: 'Playfair Display', fontSize: '30px', fontWeight: 600, lineHeight: '40px', textAlign: 'left', color: '#4D4E54' }}>
+  Our History
+</h1>
+
+<p className="leading-relaxed font-poppins mb-4 pb-10 text-lg" style={{ fontFamily: 'Poppins', fontSize: '16px', fontWeight: 400, lineHeight: '27.2px', textAlign: 'left', color: '#888888' }}>
+  Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY.
+</p>
+
             <VerticalTimeline />
           </div>
         </div>
       </section>
-      <section className="text-gray-600 body-font">    <div className="container px-5 py-24 mx-auto flex flex-wrap">
-      {sections.map((section, index) => (
-        <div key={index} className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mt-6 lg:mt-0 ml-auto">
-          <h1 className="text-black text-3xl mb-4 font-medium font-playfair">{section.title}</h1>
-          <p className="text-slate-500 mb-3 font-poppins text-lg">{section.mainContent}</p>
-          {section.subContent.map((paragraph, index) => (
-            <p key={index} className="text-slate-500 mb-3  pl-14 font-sans text-lg">{paragraph}</p>
-          ))}
-        </div>
-      ))}
-      <div className="lg:w-1/2 w-full mb-10 lg:mb-0">
-        <img className="w-full h-full object-cover object-center" src={p2} alt="person" />
+   <section className="text-gray-600 body-font">   
+  <div className="container px-5 py-24 mx-auto flex flex-wrap" >
+    {sections.map((section, index) => (
+      <div key={index} className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mt-6 lg:mt-0 ml-auto relative">
+        <h1 className="text-black text-3xl mb-4 pb-2 font-medium font-playfair" style={{ fontFamily: 'Playfair Display', fontSize: '30px', fontWeight: 600, lineHeight: '40px', textAlign: 'left', color: '#4D4E54' }}>
+          {section.title}
+        </h1>
+        <div className="absolute" style={{ border: '1px solid #A0A0A0', height: `calc(100% - ${section.subContent.length * 100}px)`, width: '2px', top: `${section.subContent.length * 65}px` }}></div>
+ <p className="text-slate-500 mb-3 pb-10  font-poppins text-lg" style={{ fontFamily: 'Poppins', fontSize: '16px', fontWeight: 400, lineHeight: '27.2px', textAlign: 'left', color: '#888888' }}>
+          {section.mainContent}
+        </p>
+        {section.subContent.map((paragraph, index) => (
+          <p key={index} className="text-slate-500 mb-3 pb-3 pl-14 font-sans text-lg" style={{ fontFamily: 'Poppins', fontSize: '16px', fontWeight: 400, lineHeight: '27.2px', textAlign: 'left', color: '#888888' }}>
+            {paragraph}
+          </p>
+        ))}
       </div>
+    ))}
+    <div className="lg:w-1/2 w-full mb-10 lg:mb-0">
+      <img className="w-full h-full object-cover object-center" src={p2} alt="person" />
     </div>
-       
-      </section>
+  </div>
+</section>
+
 
     </div>
   );
