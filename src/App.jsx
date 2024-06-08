ba2c5e74395ac6fa5bf8bacc8bbbbed6c3e58cd4
SHOBHIT SHRIVASTAVA <111689849+Shobhit51@users.noreply.github.com>
2024-06-08 10:41:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import About from './components/About'
 import Testimonials from './components/Testimonials'
+import Part from './components/Part'
 
 function App() {
 
   return (
     <div>
+        
       <About />
+     
    <Testimonials />
+   <Part></Part>
+   
+
     </div>
   );
 }
