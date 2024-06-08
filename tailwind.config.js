15647c2d796530d08eb71649fd55531c679beb18
SHOBHIT SHRIVASTAVA <111689849+Shobhit51@users.noreply.github.com>
2024-06-08 10:49:09 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
       fontFamily: {
         'playfair': ['"Playfair Display"', 'serif'],
         'poppins': ['"Poppins"', 'sans-serif'],
-         'sans': ['Montserrat', 'sans-serif'],
       },
     },
   },
