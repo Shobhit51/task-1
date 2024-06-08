9195a941a0200885a7d9bd5299178fda595fb3c7
SHOBHIT SHRIVASTAVA <111689849+Shobhit51@users.noreply.github.com>
2024-06-08 10:41:36 +0530

    Create  Part.jsx

---
+++ src/components/Part.jsx
@@ -0,0 +1,184 @@
+
+
+import React from 'react';
+
+import p4 from '../images/p4.png';
+import p5 from '../images/p5.png';
+import p7 from '../images/p7.png';
+import p8 from '../images/p8.png';
+import p9 from '../images/p9.png';
+import p13 from '../images/p13.png';
+import p15  from '../images/p15.png';
+import p12 from '../images/p12.png';
+
+import p6 from '../images/p6.png';
+function Part() {
+  return (
+    <div  style={{ paddingTop: '20px', marginBottom: '200px' }}>
+    <section style={{ paddingTop: '20px', marginBottom: '200px' }}>
+    <div  className="container mx-auto flex px-5 py-24 items-center justify-center flex-col"
+          style={{
+            width: '100%',
+            height: '700px',
+            position: 'relative',
+            left: '0px',
+            gap: '0px',
+            padding: '0',
+            background: 'linear-gradient(180deg, #000000 0%, #000000 100%)',  // Apply the linear gradient background
+            borderRadius: '35px' ,
+            opacity: '100% '// Remove padding to ensure full width
+          }}>
+
+<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'absolute', top: '50px', left: '50px' }}>
+          {/* "OUR PARTNER AND INVESTOR" div */}
+          <div style={{ width: '900px', height: '76px', fontFamily: 'Montserrat', fontSize: '48px', fontWeight: '800', lineHeight: '71.14px', textAlign: 'center', color: '#FFFFFF', marginBottom: '20px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', position: 'relative', left: '5px' }}>
+            OUR PARTNER AND INVESTOR
+          </div>
+          <div style={{fontFamily: 'Montserrat',fontSize: '18px', fontWeight: 400, lineHeight: '21.94px',textAlign: 'center',color:'white'}}>
+  Cras convallis lacus orci, tristique tincidunt magna consequat in. In vel pulvinar est, at euismod libero.
+</div>
+
+
+<section className="body-font">
+            <div className="container px-5 py-24 mx-auto">
+              <div className="flex flex-wrap -m-5 text-center justify-center">
+                <div style={{ display: 'inline-block', marginRight: '10px' }}>
+                  <img src={p5} alt="Image 1" style={{ width: '80px', height: '100px', borderRadius: '5px' }} />
+                </div>
+                <div style={{ display: 'inline-block', marginRight: '10px' }}>
+                  <img src={p6} alt="Image 1" style={{ width: '80px', height: '100px', borderRadius: '5px' }} />
+                </div>
+                <div style={{ display: 'inline-block', marginRight: '10px' }}>
+                  <img src={p7} alt="Image 1" style={{ width: '80px', height: '100px', borderRadius: '5px' }} />
+                </div>
+                <div style={{ display: 'inline-block', marginRight: '10px' }}>
+                  <img src={p8} alt="Image 1" style={{ width: '80px', height: '100px', borderRadius: '5px' }} />
+                </div>
+                <div style={{ display: 'inline-block', marginRight: '10px' }}>
+                  <img src={p9} alt="Image 1" style={{ width: '80px', height: '100px', borderRadius: '5px' }} />
+                </div>
+                <div style={{ display: 'inline-block', marginRight: '10px' }}>
+                  <img src={p12} alt="Image 1" style={{ width: '80px', height: '100px', borderRadius: '5px' }} />
+                </div>
+                <div style={{ display: 'inline-block', marginRight: '10px' }}>
+                  <img src={p13} alt="Image 1" style={{ width: '80px', height: '100px', borderRadius: '5px' }} />
+                </div>
+                <div style={{ display: 'inline-block', marginRight: '10px' }}>
+                  <img src={p15} alt="Image 1" style={{ width: '80px', height: '100px', borderRadius: '5px' }} />
+                </div>
+                <div style={{ display: 'inline-block', marginRight: '10px' }}>
+                  <img src={p13} alt="Image 1" style={{ width: '80px', height: '100px', borderRadius: '5px' }} />
+                </div>
+                <div style={{ display: 'inline-block', marginRight: '10px' }}>
+                  <img src={p12} alt="Image 1" style={{ width: '80px', height: '100px', borderRadius: '5px' }} />
+                </div>
+          
+              </div>
+            </div>
+</section>
+
+<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', width: '100%', justifyContent: 'center' }}>
+           
+           
+            <div style={{ width: '33%', marginBottom: '10px', padding: '0 10px' }}>
+              <div style={{ width: '100%', textAlign: 'center' }}>
+                <div style={{
+                   width: '201px', 
+                   height: '122px', 
+                   fontFamily: 'Montserrat', 
+                   fontSize: '100px', 
+                   fontWeight: '800', 
+                   lineHeight: '121.9px', 
+                   textAlign: 'center', 
+                   background: 'linear-gradient(180deg, #00E5FF 0%, rgba(0, 229, 255, 0.06) 91.95%)',
+                   WebkitBackgroundClip: 'text',
+                   backgroundClip: 'text',
+                   color: 'transparent',
+                   margin: '0 auto',
+                 
+                }}>
+                  123
+                </div>
+                <div  style={{ width: '283px', height: '55px', fontFamily: 'Montserrat', fontSize: '40px', fontWeight: '800', lineHeight: '38.51px', textAlign: 'center', color: '#FFFFFF' }}>
+                 Investors
+                </div>
+                <div style={{ width: '60px', height: '10px', borderRadius: '10px', background: '#00E5FF', margin: '5px auto' }}></div>
+              </div>
+            </div>
+
+
+            <div style={{ width: '33%', marginBottom: '10px', padding: '0 10px' }}>
+              <div style={{ width: '100%', textAlign: 'center' }}>
+                <div style={{
+                  width: '201px', 
+                  height: '122px', 
+                  fontFamily: 'Montserrat', 
+                  fontSize: '100px', 
+                  fontWeight: '800', 
+                  lineHeight: '121.9px', 
+                  textAlign: 'center', 
+                  background: 'linear-gradient(180deg, #D1C4E9 1.43%, rgba(49, 27, 146, 0.06) 91.95%)',
+                  WebkitBackgroundClip: 'text',
+                  backgroundClip: 'text',
+                  color: 'transparent',
+                  margin: '0 auto' 
+                }}>
+                  $123M
+                </div>
+                <div  style={{ width: '283px', height: '55px', fontFamily: 'Montserrat', fontSize: '40px', fontWeight: '800', lineHeight: '38.51px', textAlign: 'center', color: '#FFFFFF' }}>
+                  Funded
+                </div>
+                <div style={{ width: '60px', height: '10px', borderRadius: '10px', background: '#00E5FF', margin: '5px auto' }}></div>
+              </div>
+            </div>
+
+
+          
+            <div style={{ width: '33%', marginBottom: '10px', padding: '0 10px' }}>
+              <div style={{ width: '100%', textAlign: 'center' }}>
+                <div style={{
+                   width: '201px', 
+                   height: '122px', 
+                   fontFamily: 'Montserrat', 
+                   fontSize: '100px', 
+                   fontWeight: '800', 
+                   lineHeight: '121.9px', 
+                   textAlign: 'center', 
+                   background: 'linear-gradient(180deg, #00E5FF 0%, rgba(0, 229, 255, 0.06) 91.95%)',
+                   WebkitBackgroundClip: 'text',
+                   backgroundClip: 'text',
+                   color: 'transparent',
+                   margin: '0 auto' 
+                }}>
+                456
+                </div>
+                <div  style={{ width: '283px', height: '55px', fontFamily: 'Montserrat', fontSize: '40px', fontWeight: '800', lineHeight: '38.51px', textAlign: 'center', color: '#FFFFFF' }}>
+                  Partners
+                </div>
+                <div style={{ width: '60px', height: '10px', borderRadius: '10px', background: '#00E5FF', margin: '5px auto' }}></div>
+              </div>
+            </div>
+        
+            
+            </div>
+
+</div>
+
+
+
+
+
+          <img
+            className="w-full h-full object-cover object-center"
+            alt="hero"
+            src={p4}
+            style={{ width: '100%', height: '100%', borderRadius: '15px' }}  // Add borderRadius to curve corners
+            // Ensure the image covers the container
+          />
+        </div>
+        </section>
+        </div>
+   );
+}
+
+export default Part;
